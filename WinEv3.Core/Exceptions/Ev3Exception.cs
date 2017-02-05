a0907e3be82bb53ce692525a756277a3de2ba65b
Gui Priaulx <gui.priaulx@live.com.au>
2017-02-05 23:41:14 +1100

    Added base exception

---
+++ WinEv3.Core/Exceptions/Ev3Exception.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WinEv3.Core.Exceptions
+{
+    public class WinEv3Exception : Exception
+    {
+        public WinEv3Exception() {}
+
+        public WinEv3Exception(string message) : base(message) {}
+
+        public WinEv3Exception(string message, Exception inner) : base(message, inner) {}
+    }
+}
